6dc72449aaeb63d32140c8a4a4b4afe904d9900f
username <xx@gmail.com>
2021-08-20 19:22:03 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
-import {Button, Input} from 'antd'
-import 'antd/dist/antd.less'
-import {Route, Switch} from 'react-router-dom'
-import Login from './pages/login/login'
-import Admin from './pages/admin/admin'
 
 export default class About extends Component{
   render(){
     return (
-      <Switch>
-        <Route path="/login" component={Login}/>
-        <Route path="/admin" component={Admin}/>
-      </Switch>
-      
+      <h1>
+        App
+      </h1>
     )
   }
 }
